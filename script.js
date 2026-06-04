2dfc4bdaba48ff46ebcaf8af34b20f7505b8efd1
AssiaCherfaoui <assiacherfaoui44@gmail.com>
2026-06-04 13:34:57 +0200

    ajout des cookies banderole/transition/media queries

---
+++ script.js
@@ -0,0 +1,21 @@
+const cookieBox = document.querySelector(".contentCookie");
+const buttons = document.querySelectorAll(".button");
+console.log(cookieBox, buttons);
+
+function executesCodes() {
+    // if (document.cookie.includes("true")) return;
+    cookieBox.classList.add("show");
+
+
+    buttons.forEach(button => {
+        button.addEventListener("click", () => {
+
+            cookieBox.classList.remove("show");
+            if (button.id == "accept") {
+                document.cookie = "rgpd_consent=true; max-age=" + 60 * 60 * 24 * 30;
+            }
+        })
+    })
+};
+
+window.addEventListener("load", executesCodes)
